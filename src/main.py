a792790bafe59a675f59c838c52733c8afe70df0
502657 <fedyahonest@gmail.com>
2025-10-08 11:42:54 +0300

    task1+task2

--- src/main.py
+++ src/main.py
@@ -1,1 +1,22 @@
-print('lab5')
+import re
+
+
+
+
+#task1
+
+# text='ададд и лаба и дадад и сс'
+import re
+
+text = open('task1-ru.txt', encoding='utf-8').read()
+
+print(re.findall(r'\bс[а-яё]+', text, flags=re.I))
+print(re.findall(r'\bи\s+([а-яё]+)', text, flags=re.I))
+
+#task2
+
+html = open('task2.html', encoding='utf-8').read()
+print( re.findall(r'font-family\s*:\s*([^;}{]+)', html, flags=re.I))
+
+#task3
+
