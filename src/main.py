a68953e4c7c893f2b25812c49046e93071878ff0
502657 <fedyahonest@gmail.com>
2025-10-08 17:30:32 +0300

    last commit

--- src/main.py
+++ src/main.py
@@ -5,9 +5,6 @@ import re
 
 #task1
 
-# text='ададд и лаба и дадад и сс'
-import re
-
 text = open('task1-ru.txt', encoding='utf-8').read()
 
 print(re.findall(r'\bс[а-яё]+', text, flags=re.I))
@@ -16,4 +13,5 @@ print(re.findall(r'\bи\s+([а-яё]+)', text, flags=re.I))
 #task2
 
 html = open('task2.html', encoding='utf-8').read()
-print( re.findall(r'font-family(.*)', html, flags=re.I))
+print(re.findall(r"font-family\s*:\s*'([^']+)'", html)
+)
--- task3.py
+++ task3.py
@@ -2,13 +2,13 @@ import re, csv
 
 s = open('task3.txt', encoding='utf-8').read().split()
 
-id = [st for st in s if re.fullmatch(r'\d+', st)]
+ids = [st for st in s if re.fullmatch(r'\d+', st)]
 fam = [st for st in s if re.fullmatch(r'[A-Z][a-zA-Z]+', st)]
 emails = [st for st in s if re.fullmatch(r'\S+@\S+', st)]
 date = [st for st in s if re.fullmatch(r'\d{4}-\d{2}-\d{2}', st)]
 web = [st for st in s if re.fullmatch(r'https?://\S+|www\.\S+', st)]
 
-rows = [[id[i], fam[i], emails[i], date[i], web[i]] for i in range(len(id))]
+rows = [[ids[i], fam[i], emails[i], date[i], web[i]] for i in range(len(ids))]
 
 #savecsv
 with open('task3_out.csv','w', encoding='utf-8', newline='') as f:
